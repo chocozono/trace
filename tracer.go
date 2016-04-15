a7186937e936796c716f02545dab6db42f3a315b
chocozono <chocozono@gmail.com>
2016-04-15 18:16:46 +0900

    Initial commit

--- tracer.go
+++ tracer.go
@@ -1,6 +1,18 @@
 package trace
 
+import "io"
+
 //Tracer is the interface that can record some events
 type Tracer interface {
 	Trace(...interface{})
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {}
+
+func New(w io.Writer) Tracer {
+	return nil
 }
---
+++ tracer_test.go
@@ -0,0 +1,19 @@
+package trace
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Return value from New() is null")
+	} else {
+		tracer.Trace("Hello, trace package\n")
+		if buf.String() != "Hello, trace package\n" {
+			t.Errorf("Wrong output value: '%s'", buf.String())
+		}
+	}
+}
